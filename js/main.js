b7614e72d51c4bb8adad0c4e8f8cb318d3ebe737
Martina Amore <martina.amore02@gmail.com>
2020-11-19 18:06:00 +0100

    Bonus funzionante

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@ const root = new Vue({
     data: {
         itemUtente: "",
         itemLista: [],
-        isClicked: true,
+        isClicked: []
     },
     methods: {
         addItem: function() {
             this.itemLista.push(this.itemUtente);
             this.itemUtente = "";
+            this.isClicked.push(false);
         },
         removeItem: function(cancelIndex){
 			this.itemLista.splice(cancelIndex, 1);
+            this.isClicked.splice(cancelIndex, 1);
 		},
         changeColor: function(index){
             console.log(this.isClicked);
-            this.isClicked = !this.isClicked;
+            Vue.set(this.isClicked, index, !this.isClicked[index]);
 		},
         display: function() {
             if (this.itemLista.length === 0) {
